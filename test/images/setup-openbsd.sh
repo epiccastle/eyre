88b76211c4e3f8750b03e2f0f2180d4d9dc51d61
Crispin Wellington <retrogradeorbit@gmail.com>
2026-06-19 17:15:44 +0800

    BSD image preparation scripts

---
+++ test/images/setup-openbsd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+expect << 'EOF'
+set timeout 120
+spawn socat - UNIX-CONNECT:/tmp/qemu-0.sock
+expect "login:"
+send "root\r"
+expect ":"
+send "testpassword\r"
+expect "# "
+send "shutdown -p now\r"
+expect eof
+EOF
