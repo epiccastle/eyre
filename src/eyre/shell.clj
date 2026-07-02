c9d64378ba5642cc230d92a4de135dd60e01a936
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-02 22:42:48 +0800

    break out scripts into top level vars

--- src/eyre/shell.clj
+++ src/eyre/shell.clj
@@ -8,49 +8,16 @@
 
 (def newlines #"\r\n|\n\r|\r|\n")
 
-(defn process-version-line [version-line]
-  (prn version-line)
-  (-> version-line
-      (str/split #":")
-      (->> (partition 2)
-           (keep (fn [[k v]]
-                  (when (pos? (count v))
-                    [({"B" :bash
-                       "Z" :zsh
-                       "F" :fish
-                       "K" :ksh} k)
-                     (str/trim v)]))))
-      first))
+(def check-cmd-type-script "echo %COMSPEC%\r\nWrite-Output powershell\r\necho $0\r\n")
 
+(def ver-script "ver")
 
-#_ (process-version-line "B:3.2.57(1)-release:Z::F::K:")
-#_ (process-version-line "B::Z::F::K:")
-#_ (process-version-line "B::Z::F:3.7.0:K:")
+(def powershell-version-path-script "echo $PSVersionTable.PSVersion
+echo path:
+echo (Get-Process -Id $PID).Path
+")
 
-(defn process-powershell [version-lines]
-  (let [[header _ version] (-> version-lines
-                               str/trim
-                               (str/split newlines))
-        header (-> header
-                   str/trim
-                   (str/split #"\s+")
-                   (->> (map str/lower-case)
-                        (map keyword)))
-        version-parts (-> version
-                          str/trim
-                          (str/split #"\s+"))
-        version (->> (map vector header version-parts)
-                     (into {}))
-        ]
-    (str (:major version) "." (:minor version) "." (:build version) "." (:revision version))))
-
-#_ (process-powershell "\r\nMajor  Minor  Build  Revision\r\n-----  -----  -----  --------\r\n5      1      20348  558     \r\n\r\n\r\n")
-
-(defn determine-shell [{:keys [exec]}]
-  (let [{:keys [exit out err]} (exec "echo %COMSPEC%\r\nWrite-Output powershell\r\necho $0\r\n")]
-    (if (and (= 1 exit) (str/includes? err "variable not found") (str/includes? err "nu::parser::variable_not_found"))
-      ;; nushell
-      (let [{:keys [exit out err]} (exec "nu --version
+(def nushell-version-script "nu --version
 print $env.SHELL
 
 # Try to resolve the real path of $SHELL
@@ -83,62 +50,13 @@ let resolved = if (which greadlink | is-not-empty) {
 
 print $resolved
 
-"
+")
 
-                                         )]
-        (assert (zero? exit) (str "nushell version determination exited non zero: " exit " " err))
-        (let [[version shell path] (str/split out newlines)]
-          {:type :nu
-           :version version
-           :shell shell
-           :canonical-path path}))
-
-      ;; other
-      (do
-        (assert (zero? exit) (str "shell determination script 1 exited non zero: " exit " " err))
-        (let [[line-1 line-2 & remain] (str/split out newlines)
-              first-guess (cond
-                            (not= line-1 "%COMSPEC%") :cmd.exe
-                            (= line-2 "powershell") :powershell
-                            :else (keyword line-2))]
-          (case first-guess
-            :cmd.exe
-            (let [{:keys [exit out err]} (exec "ver")
-                  version (second (re-find #"[vV]ersion ([\d.]+)" out))]
-              {:type :cmd-exe
-               :version version
-               :shell line-1
-               :path line-1})
-
-            :powershell
-            (let [{:keys [exit out err]} (exec "echo $PSVersionTable.PSVersion
-echo path:
-echo (Get-Process -Id $PID).Path
-")]
-              (let [[version path] (str/split out #"\r\npath:\r\n")
-                    path (str/trim path)]
-                {:type :powershell
-                 :version (process-powershell version)
-                 :shell path
-                 :canonical-path path}))
-
-            ;; bash like shell
-            (let [{:keys [exit out err]} (exec "echo \"B:$BASH_VERSION:Z:$ZSH_VERSION:F:$FISH_VERSION:K:$KSH_VERSION\"
+(def bash-versions-script "echo \"B:$BASH_VERSION:Z:$ZSH_VERSION:F:$FISH_VERSION:K:$KSH_VERSION\"
 echo shell:$SHELL
-")]
-              (assert (zero? exit) (str "shell determination script 2 exited non zero: " exit " " err))
-              (let [[versions shell] (str/split out newlines)
-                    shell (second (str/split shell #"shell:"))
-                    versions (process-version-line versions)
-                    [shell-type shell-version] versions
-                    ]
+")
 
-                (prn 'SHELL versions)
-                (let [{:keys [exit out err]}
-                      (exec
-                        (case shell-type
-                          :fish
-                          "set target (command -v $SHELL)
+(def fish-canonical-path-script "set target (command -v $SHELL)
 while test -L $target
     set link (readlink $target)
     if string match -q '/*' $link
@@ -147,10 +65,9 @@ while test -L $target
         set target (dirname $target)/$link
     end
 end
-cd (dirname $target); and echo (pwd -P)/(basename $target)"
+cd (dirname $target); and echo (pwd -P)/(basename $target)")
 
-                          ;; bash like shells
-                          " if command -v greadlink >/dev/null 2>&1; then
+(def default-canonical-path-script " if command -v greadlink >/dev/null 2>&1; then
   greadlink -f \"$(command -v $SHELL)\"
 elif readlink -f / >/dev/null 2>&1; then
   readlink -f \"$(command -v $SHELL)\"
@@ -166,27 +83,9 @@ else
   done
   cd -- \"$(dirname -- \"$target\")\" && echo \"$(pwd -P)/$(basename -- \"$target\")\"
 fi
-"))]
-                  (assert (zero? exit) (str "shell determination script 3 exited non zero: " exit " " err))
-                  (let [[sh-readline] (str/split out newlines)
-                        busybox? (str/ends-with? sh-readline "/busybox")
-                        dash? (str/ends-with? sh-readline "/dash")]
-                    (cond
-                      busybox?
-                      (let [{:keys [exit out err]}
-                            (exec (str sh-readline " --help 2>&1 | head -1"))
-                            version (second (str/split out #"\s+"))
-                            ]
-                        {:type :busybox
-                         :version version
-                         :shell shell
-                         :canonical-path sh-readline}
-                        )
+")
 
-                      dash?
-                      (let [{:keys [exit out err]}
-                            (exec
-                              "#!/bin/dash
+(def dash-version-script "#!/bin/dash
 
 # Try to determine the version of dash currently running,
 # using multiple methods across different Linux distros.
@@ -256,6 +155,111 @@ else
     exit 1
 fi
 ")
+
+(defn process-version-line [version-line]
+  (prn version-line)
+  (-> version-line
+      (str/split #":")
+      (->> (partition 2)
+           (keep (fn [[k v]]
+                  (when (pos? (count v))
+                    [({"B" :bash
+                       "Z" :zsh
+                       "F" :fish
+                       "K" :ksh} k)
+                     (str/trim v)]))))
+      first))
+
+#_ (process-version-line "B:3.2.57(1)-release:Z::F::K:")
+#_ (process-version-line "B::Z::F::K:")
+#_ (process-version-line "B::Z::F:3.7.0:K:")
+
+(defn process-powershell [version-lines]
+  (let [[header _ version] (-> version-lines
+                               str/trim
+                               (str/split newlines))
+        header (-> header
+                   str/trim
+                   (str/split #"\s+")
+                   (->> (map str/lower-case)
+                        (map keyword)))
+        version-parts (-> version
+                          str/trim
+                          (str/split #"\s+"))
+        version (->> (map vector header version-parts)
+                     (into {}))]
+    (str (:major version) "." (:minor version) "." (:build version) "." (:revision version))))
+
+#_ (process-powershell "\r\nMajor  Minor  Build  Revision\r\n-----  -----  -----  --------\r\n5      1      20348  558     \r\n\r\n\r\n")
+
+(defn determine-shell [{:keys [exec]}]
+  (let [{:keys [exit out err]} (exec check-cmd-type-script)]
+    (if (and (= 1 exit) (str/includes? err "variable not found") (str/includes? err "nu::parser::variable_not_found"))
+      ;; nushell
+      (let [{:keys [exit out err]} (exec nushell-version-script)]
+        (assert (zero? exit) (str "nushell version determination exited non zero: " exit " " err))
+        (let [[version shell path] (str/split out newlines)]
+          {:type :nu
+           :version version
+           :shell shell
+           :canonical-path path}))
+
+      ;; other
+      (do
+        (assert (zero? exit) (str "shell determination script 1 exited non zero: " exit " " err))
+        (let [[line-1 line-2 & remain] (str/split out newlines)
+              first-guess (cond
+                            (not= line-1 "%COMSPEC%") :cmd.exe
+                            (= line-2 "powershell") :powershell
+                            :else (keyword line-2))]
+          (case first-guess
+            :cmd.exe
+            (let [{:keys [exit out err]} (exec ver-script)
+                  version (second (re-find #"[vV]ersion ([\d.]+)" out))]
+              {:type :cmd-exe
+               :version version
+               :shell line-1
+               :path line-1})
+
+            :powershell
+            (let [{:keys [exit out err]} (exec powershell-version-path-script)]
+              (let [[version path] (str/split out #"\r\npath:\r\n")
+                    path (str/trim path)]
+                {:type :powershell
+                 :version (process-powershell version)
+                 :shell path
+                 :canonical-path path}))
+
+            ;; bash like shell
+            (let [{:keys [exit out err]} (exec bash-versions-script)]
+              (assert (zero? exit) (str "shell determination script 2 exited non zero: " exit " " err))
+              (let [[versions shell] (str/split out newlines)
+                    shell (second (str/split shell #"shell:"))
+                    versions (process-version-line versions)
+                    [shell-type shell-version] versions]
+                (let [{:keys [exit out err]}
+                      (exec
+                        (case shell-type
+                          :fish fish-canonical-path-script
+                          ;; bash like shells
+                          default-canonical-path-script))]
+                  (assert (zero? exit) (str "shell determination script 3 exited non zero: " exit " " err))
+                  (let [[sh-readline] (str/split out newlines)
+                        busybox? (str/ends-with? sh-readline "/busybox")
+                        dash? (str/ends-with? sh-readline "/dash")]
+                    (cond
+                      busybox?
+                      (let [{:keys [exit out err]}
+                            (exec (str sh-readline " --help 2>&1 | head -1"))
+                            version (second (str/split out #"\s+"))]
+                        {:type :busybox
+                         :version version
+                         :shell shell
+                         :canonical-path sh-readline})
+
+                      dash?
+                      (let [{:keys [exit out err]}
+                            (exec dash-version-script)
                             version (-> out
                                         str/trim
                                         (str/split #"dash version:\s*")
