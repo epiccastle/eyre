137a654fc0b1872bc2be2dafb18d750ae3d96ca6
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-02 17:15:14 +0800

    follow nushell links to find canonical path

--- test/eyre_test/execute.clj
+++ test/eyre_test/execute.clj
@@ -204,9 +204,48 @@
   (let [{:keys [exit out err]} (exec "echo %COMSPEC%\r\nWrite-Output powershell\r\necho $0\r\n")]
     (if (and (= 1 exit) (str/includes? err "variable not found") (str/includes? err "nu::parser::variable_not_found"))
       ;; nushell
-      (let [{:keys [exit out err]} (exec "nu --version")]
+      (let [{:keys [exit out err]} (exec "nu --version
+print $env.SHELL
+
+# Try to resolve the real path of $SHELL
+let shell_path = (which $env.SHELL | get path.0? | default $env.SHELL)
+
+let resolved = if (which greadlink | is-not-empty) {
+    # Use greadlink -f if available (macOS with coreutils)
+    ^greadlink -f $shell_path
+} else {
+    # Use Nu's built-in path canonicalize (resolves symlinks like readlink -f)
+    try {
+        $shell_path | path expand --strict
+    } catch {
+        # BSD fallback: manually loop-resolve symlinks
+        mut target = $shell_path
+        loop {
+            let meta = (ls -la $target | get 0)
+            if $meta.type != \"symlink\" { break }
+
+            let link = (readlink $target)  # or: ^readlink $target
+            $target = if ($link | str starts-with \"/\") {
+                $link
+            } else {
+                ($target | path dirname) | path join $link
+            }
+        }
+        $target
+    }
+}
+
+print $resolved
+
+"
+
+                                         )]
         (assert (zero? exit) (str "nushell version determination exited non zero: " exit " " err))
-        [:nu (str/trim out)])
+        (let [[version shell path] (str/split out #"\r\n|\n\r|\r|\n")]
+          {:type :nu
+           :version version
+           :shell shell
+           :canonical-path path}))
 
       ;; other
       (do
