0e292ce5be0bcf9d837e49e02772c5f50b14440d
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-02 16:45:46 +0800

    add other shells to test execute setu p

--- test/eyre_test/execute.clj
+++ test/eyre_test/execute.clj
@@ -9,25 +9,97 @@
 (def host-ports
   {
    ;; qemu hosts
-   :windows     {:port 22001
-                 :username "Administrator"}
+   ;;#_#_
+   :windows {:port     22001
+             :username "Administrator"}
 
    ;; openbsd doesnt boot, WARNING: / was not properly unmounted
-   #_#_:openbsd     22002
+   #_#_:openbsd 22002
 
-   :netbsd      {:port 22003}
-   :freebsd     {:port 22004}
-   :macos       {:port 22005}
+   ;;#_#_#_#_#_#_
+   :netbsd  {:port 22003}
+   :freebsd {:port 22004}
+   :macos   {:port 22005}
 
    ;; docker hosts
+   ;;#_#_#_#_#_#_#_#_
    :alpine      {:port 22020}
+   :alpine-fish {:port     22020
+                 :username "fish"}
+   :alpine-zsh  {:port     22020
+                 :username "zsh"}
+   :alpine-dash {:port     22020
+                 :username "dash"}
+
+   ;;#_#_#_#_#_#_#_#_#_#_
    :ubuntu      {:port 22021}
+   :ubuntu-fish {:port     22021
+                 :username "fish"}
+   :ubuntu-zsh  {:port     22021
+                 :username "zsh"}
+   :ubuntu-dash {:port     22021
+                 :username "dash"}
+   :ubuntu-ksh {:port     22021
+                :username "ksh"}
+
+   ;;#_#_#_#_#_#_#_#_#_#_
    :debian      {:port 22022}
+   :debian-fish {:port     22022
+                 :username "fish"}
+   :debian-zsh  {:port     22022
+                 :username "zsh"}
+   :debian-dash {:port     22022
+                 :username "dash"}
+   :debian-ksh {:port     22022
+                :username "ksh"}
+
+   ;;#_#_#_#_#_#_#_#_#_#_
    :fedora      {:port 22023}
+   :fedora-fish {:port     22023
+                 :username "fish"}
+   :fedora-zsh  {:port     22023
+                 :username "zsh"}
+   :fedora-dash {:port     22023
+                 :username "dash"}
+   :fedora-ksh {:port     22023
+                :username "ksh"}
+   :fedora-nu {:port     22023
+                :username "nu"}
+
+   ;;#_#_#_#_#_#_#_#_#_#_#_#_
    :archlinux   {:port 22024}
+   :archlinux-fish {:port     22024
+                    :username "fish"}
+   :archlinux-zsh  {:port     22024
+                    :username "zsh"}
+   :archlinux-dash {:port     22024
+                    :username "dash"}
+   :archlinux-ksh {:port     22024
+                   :username "ksh"}
+   :archlinux-nu {:port     22024
+                   :username "nu"}
+
+   ;;#_#_#_#_#_#_
    :amazonlinux {:port 22025}
+   :amazonlinux-zsh  {:port     22025
+                      :username "zsh"}
+   :amazonlinux-ksh {:port     22025
+                     :username "ksh"}
+
+   ;;#_#_#_#_#_#_
    :rockylinux  {:port 22026}
-   :oraclelinux {:port 22027}})
+   :rockylinux-zsh  {:port     22026
+                     :username "zsh"}
+   :rockylinux-ksh {:port     22026
+                    :username "ksh"}
+
+   ;;#_#_#_#_#_#_
+   :oraclelinux {:port 22027}
+   :oraclelinux-zsh  {:port     22027
+                      :username "zsh"}
+   :oraclelinux-ksh {:port     22027
+                     :username "ksh"}
+   })
 
 (defn test-ports
   []
@@ -91,13 +163,14 @@
    "4529  -  Ss   0:00.00 sh -c echo B:$BASH_VERSION:Z:$ZSH_VERSION:F:$FISH_VERSION:K:$KSH_VERSION\\necho ps:\\nps -p $$ || echo no-ps\\necho sh:\\nif command -v greadlink >/dev/null 2>&1; then\\n  greadlink -f \"$(command -v sh)\"\\nelif readlink -f / >/dev/null 2>&1; then\\n  readlink -f \"$(command -v sh)\"\\nelse\\n  # BSD readlink fallback: manually loop-resolve\\n  target=\"$(command -v sh)\"\\n  while [ -L \"$target\" ]; do\\n    link=\"$(readlink \"$target\")\"\\n    case \"$link\" in\\n      /*) target=\"$link\" ;;\\n      *) target=\"$(dirname \"$target\")/$link\" ;;\\n    esac\\n  done\\n  cd -- \"$(dirname -- \"$target\")\" && echo \"$(pwd -P)/$(basename -- \"$target\")\"\\nfi\\n"))
 
 (defn process-version-line [version-line]
+  (prn version-line)
   (-> version-line
       (str/split #":")
       (->> (partition 2)
            (keep (fn [[k v]]
                   (when (pos? (count v))
                     [({"B" :bash
-                       "Z" :zfs
+                       "Z" :zsh
                        "F" :fish
                        "K" :ksh} k)
                      (str/trim v)]))))
@@ -106,6 +179,7 @@
 
 #_ (process-version-line "B:3.2.57(1)-release:Z::F::K:")
 #_ (process-version-line "B::Z::F::K:")
+#_ (process-version-line "B::Z::F:3.7.0:K:")
 
 (defn process-powershell [version-lines]
   (let [[header _ version] (-> version-lines
@@ -128,25 +202,59 @@
 
 (defn determine-shell [{:keys [exec]}]
   (let [{:keys [exit out err]} (exec "echo %COMSPEC%\r\nWrite-Output powershell\r\necho $0\r\n")]
-    (assert (zero? exit) (str "shell determination script 1 exited non zero: " exit " " err))
-    (let [[line-1 line-2 & remain] (str/split out #"\r\n|\n\r|\r|\n")
-          first-guess (cond
-                        (not= line-1 "%COMSPEC%") :cmd.exe
-                        (= line-2 "powershell") :powershell
-                        :else (keyword line-2))]
-      (case first-guess
-        :cmd.exe
-        first-guess
+    (if (and (= 1 exit) (str/includes? err "variable not found") (str/includes? err "nu::parser::variable_not_found"))
+      ;; nushell
+      (let [{:keys [exit out err]} (exec "nu --version")]
+        (assert (zero? exit) (str "nushell version determination exited non zero: " exit " " err))
+        [:nu (str/trim out)])
 
-        :powershell
-        (let [{:keys [exit out err]} (exec "echo $PSVersionTable.PSVersion")]
-          [:powershell (process-powershell out)])
+      ;; other
+      (do
+        (assert (zero? exit) (str "shell determination script 1 exited non zero: " exit " " err))
+        (let [[line-1 line-2 & remain] (str/split out #"\r\n|\n\r|\r|\n")
+              first-guess (cond
+                            (not= line-1 "%COMSPEC%") :cmd.exe
+                            (= line-2 "powershell") :powershell
+                            :else (keyword line-2))]
+          (case first-guess
+            :cmd.exe
+            first-guess
 
-        ;; bash like shell
-        (let [{:keys [exit out err]} (exec "echo B:$BASH_VERSION;Z:$ZSH_VERSION:F:$FISH_VERSION:K:$KSH_VERSION
+            :powershell
+            (let [{:keys [exit out err]} (exec "echo $PSVersionTable.PSVersion")]
+              [:powershell (process-powershell out)])
+
+            ;; bash like shell
+            (let [{:keys [exit out err]} (exec "echo \"B:$BASH_VERSION:Z:$ZSH_VERSION:F:$FISH_VERSION:K:$KSH_VERSION\"
 echo shell:$SHELL
-echo ps:
-ps -p $$ || echo no-ps
+")]
+              (assert (zero? exit) (str "shell determination script 2 exited non zero: " exit " " err))
+              (let [[versions shell] (str/split out #"\r\n|\n\r|\r|\n")
+                    shell (second (str/split shell #"shell:"))
+                    versions (process-version-line versions)
+                    [shell-type shell-version] versions
+                    ]
+
+                (prn 'SHELL versions)
+                (let [{:keys [exit out err]}
+                      (exec
+                        (case shell-type
+                          :fish
+                          "ps -p $fish_pid; or echo no-ps
+echo \"sh:\"
+set target (command -v $SHELL)
+while test -L $target
+    set link (readlink $target)
+    if string match -q '/*' $link
+        set target $link
+    else
+        set target (dirname $target)/$link
+    end
+end
+cd (dirname $target); and echo (pwd -P)/(basename $target)"
+
+                          ;; bash like shells
+                          "ps -p $$ || echo no-ps
 echo sh:
 if command -v greadlink >/dev/null 2>&1; then
   greadlink -f \"$(command -v $SHELL)\"
@@ -164,17 +272,16 @@ else
   done
   cd -- \"$(dirname -- \"$target\")\" && echo \"$(pwd -P)/$(basename -- \"$target\")\"
 fi
-")]
-          (assert (zero? exit) (str "shell determination script 2 exited non zero: " exit " " err))
-          (let [[[versions shell] _ ps _ [sh-readline]] (->> (str/split out #"\r\n|\n\r|\r|\n")
-                                                       (partition-by #{"ps:" "sh:"})
-                                                       )
-                shell (second (str/split shell #"shell:"))
-                ps (if (= ["no-ps"] ps)
-                     nil
-                     (process-ps ps))
-                versions (process-version-line versions)]
-            [shell versions ps sh-readline]))))))
+"))]
+                  (assert (zero? exit) (str "shell determination script 3 exited non zero: " exit " " err))
+                  (let [[ps _ [sh-readline]] (->> (str/split out #"\r\n|\n\r|\r|\n")
+                                                  (partition-by #{"sh:"}))
+                        ps (if (= ["no-ps"] ps)
+                             nil
+                             (process-ps ps))]
+                    [shell versions ps sh-readline]))))
+
+            ))))))
 
 (defn run-all [func]
   (->>
@@ -186,7 +293,7 @@ fi
                   (prn host)
                   (try
                     (let [session (ssh/ssh "localhost" (merge
-                                                         {:username "root"
+                                                         {:username (:username conf "root")
                                                           :password "root-access-please"
                                                           :strict-host-key-checking false}
                                                          conf))
