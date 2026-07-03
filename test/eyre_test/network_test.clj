2359189bfdd83ff559d40788c0c3efd6e8e85221
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-03 23:43:03 +0800

    some simple tests while refactoring

--- test/eyre_test/network_test.clj
+++ test/eyre_test/network_test.clj
@@ -7,9 +7,121 @@
             [clojuressh.core :as ssh]
             [clojuressh.session :as session]))
 
-(into {}
-      (for [host [:windows :macos :freebsd :ubuntu]]
-        (let [exec (shell-test/make-executor-fn (shell-test/host-ports host))]
-          [host (network/determine-network
-                  {:exec exec
-                   :shell (shell/determine-shell {:exec exec})})])))
+(deftest network-test
+  (is (=
+        (into {}
+              (for [host [:windows :macos :freebsd :ubuntu]]
+                (let [exec (shell-test/make-executor-fn (shell-test/host-ports host))]
+                  [host (network/determine-network
+                          {:exec exec
+                           :shell (shell/determine-shell {:exec exec})})])))
+        {:windows
+         {:hostname "WINTEST",
+          :interfaces
+          '({:name "Ethernet",
+            :ipv4 [{:address "10.0.2.15", :prefix 24}],
+            :ipv6
+            [{:address "fec0::84ec:5be3:8c22:c04f%1", :prefix 64}
+             {:address "fe80::84ec:5be3:8c22:c04f%6", :prefix 64}],
+            :loopback false,
+            :status :up,
+            :mtu nil,
+            :mac "52:54:00:12:34:56"}
+           {:name "Loopback Pseudo-Interface 1",
+            :ipv4 [{:address "127.0.0.1", :prefix 8}],
+            :ipv6 [{:address "::1", :prefix 128}],
+            :loopback true,
+            :status :up,
+            :mtu 4294967295}),
+          :default-gateway {:address "10.0.2.2", :interface "Ethernet"},
+          :dns {:nameservers [], :search []}},
+         :macos
+         {:hostname "iMac.local",
+          :interfaces
+          '({:ipv4 [{:address "127.0.0.1", :prefix 8}],
+            :ipv6 [{:address "::1", :prefix 128}],
+            :name "lo0",
+            :mac "00:00:00:00:00:00",
+            :mtu 16384,
+            :status :up,
+            :loopback true}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "EHC253",
+            :mac nil,
+            :mtu 0,
+            :status :down,
+            :loopback false}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "gif0",
+            :mac nil,
+            :mtu 1280,
+            :status :down,
+            :loopback false}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "UHC29",
+            :mac nil,
+            :mtu 0,
+            :status :down,
+            :loopback false}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "stf0",
+            :mac nil,
+            :mtu 1280,
+            :status :down,
+            :loopback false}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "UHC93",
+            :mac nil,
+            :mtu 0,
+            :status :down,
+            :loopback false}
+           {:ipv4 [],
+            :ipv6 [],
+            :name "UHC61",
+            :mac nil,
+            :mtu 0,
+            :status :down,
+            :loopback false}
+           {:ipv4 [{:address "10.0.2.15", :prefix 24}],
+            :ipv6
+            [{:address "fec0::14e9:2943:cac0:7d4e", :prefix 64}
+             {:address "fec0::5cc2:73a1:4342:494e", :prefix 64}
+             {:address "fec0::acd9:ccf2:b8d2:7719", :prefix 64}
+             {:address "fec0::f58c:9398:e289:fbcf", :prefix 64}],
+            :name "en0",
+            :mac "52:54:00:c9:18:27",
+            :mtu 1500,
+            :status :up,
+            :loopback false}),
+          :default-gateway {:address "10.0.2.2", :interface "en0"},
+          :dns {:nameservers ["10.0.2.3"], :search []}},
+         :freebsd
+         {:hostname "",
+          :interfaces
+          '({:ipv4 [{:address "10.0.2.15", :prefix 24}],
+            :ipv6 [],
+            :name "vtnet0",
+            :mac "52:54:00:12:34:56",
+            :mtu 1500,
+            :status :up,
+            :loopback false}
+           {:ipv4 [{:address "127.0.0.1", :prefix 8}],
+            :ipv6 [{:address "::1", :prefix 128}],
+            :name "lo0",
+            :mac "00:00:00:00:00:00",
+            :mtu 16384,
+            :status :up,
+            :loopback true}),
+          :default-gateway {:address "10.0.2.2", :interface "vtnet0"},
+          :dns {:nameservers ["10.0.2.3"], :search []}},
+         :ubuntu
+         {:hostname "a3603551e18c",
+          :interfaces [],
+          :default-gateway nil,
+          :dns {:nameservers ["192.168.92.2"], :search ["plume"]}}}
+        )))
--- test/eyre_test/os_test.clj
+++ test/eyre_test/os_test.clj
@@ -7,9 +7,56 @@
             [clojuressh.core :as ssh]
             [clojuressh.session :as session]))
 
-(into {}
-      (for [host [:windows :macos :freebsd :ubuntu]]
-        (let [exec (shell-test/make-executor-fn (shell-test/host-ports host))]
-          [host (os/determine-os
-                  {:exec exec
-                   :shell (shell/determine-shell {:exec exec})})])))
+(deftest os-selection
+  (is (=
+        (into {}
+              (for [host [:windows :macos :freebsd :ubuntu]]
+                (let [exec (shell-test/make-executor-fn (shell-test/host-ports host))]
+                  [host (os/determine-os
+                          {:exec exec
+                           :shell (shell/determine-shell {:exec exec})})])))
+        {:windows
+         {:family :windows,
+          :kernel {:name "Windows", :release "10.0.20348.587"},
+          :machine "x86_64",
+          :distro
+          {:id :windows,
+           :caption "Microsoft Windows Server 2022 Standard Evaluation",
+           :release "10.0.20348",
+           :build "20348"}},
+         :macos
+         {:family :darwin,
+          :kernel
+          {:name "Darwin",
+           :release "19.6.0",
+           :version
+           "Darwin Kernel Version 19.6.0: Thu Oct 29 22:56:45 PDT 2020; root:xnu-6153.141.2.2~1/RELEASE_X86_64"},
+          :machine "x86_64",
+          :distro
+          {:id :macos,
+           :name "Mac OS X",
+           :release "10.15.7",
+           :codename :catalina,
+           :build "19H15"}},
+         :freebsd
+         {:family :freebsd,
+          :kernel
+          {:name "FreeBSD",
+           :release "15.1-RELEASE",
+           :version
+           "FreeBSD 15.1-RELEASE releng/15.1-n283562-96841ea08dcf GENERIC"},
+          :machine "amd64"},
+         :ubuntu
+         {:family :linux,
+          :kernel
+          {:name "Linux",
+           :release "6.12.91-1-MANJARO",
+           :version
+           "#1 SMP PREEMPT_DYNAMIC Sun, 24 May 2026 05:07:21 +0000"},
+          :machine "x86_64",
+          :distro
+          {:id :ubuntu,
+           :name "Ubuntu",
+           :release "24.04",
+           :codename :noble,
+           :description "Ubuntu 24.04.4 LTS"}}})))
