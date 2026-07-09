185f77cde8e4ba53bb1551113b3ef514d90b181f
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-09 23:00:46 +0800

    misnamed block

--- src/eyre/network/gather.sh
+++ src/eyre/network/gather.sh
@@ -18,6 +18,6 @@ echo '===sys-class-net==='
 grep -r "" /sys/class/net/*/ 2>/dev/null
 echo '===proc-net-route==='
 cat /proc/net/route 2>/dev/null
-echo '===proc-net-fib-trie-==='
+echo '===proc-net-fib-trie==='
 cat /proc/net/fib_trie 2>/dev/null
 echo '===end==='
