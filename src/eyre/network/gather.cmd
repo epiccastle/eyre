443aa6c5d827280276cbd12e6597052d3aa4a096
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-14 00:13:55 +0800

    fix problem of clearing stdin on console connect to individual cmd.exe commands

--- src/eyre/network/gather.cmd
+++ src/eyre/network/gather.cmd
@@ -1,7 +1,1 @@
-echo ===hostname===
-hostname
-echo ===ipconfig===
-ipconfig /all
-echo ===route-print===
-route print 0.0.0.0
-echo ===end===
+(echo ===hostname===&hostname&echo ===ipconfig===&ipconfig /all&echo ===route-print===&route print 0.0.0.0&echo ===end===)
