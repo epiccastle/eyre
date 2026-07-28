3e1bd10726526e326c6e60e05adae68364cd0f8c
Crispin Wellington <retrogradeorbit@gmail.com>
2026-07-28 12:39:54 +0800

    fix ssh setup on freebsd

--- test/systems/freebsd/http/install.sh
+++ test/systems/freebsd/http/install.sh
@@ -5,7 +5,7 @@ BSDINSTALL_LOG="/tmp/install.log"
 #!/bin/sh
 sysrc ifconfig_DEFAULT=DHCP
 sysrc sshd_enable=YES
-sed -i'' -E 's/^[[:blank:]]*#?[[:blank:]]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i'' -E 's/^[[:blank:]]*#?[[:blank:]]*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "root-access-please" | pw usermod root -h 0
 shutdown -r now
